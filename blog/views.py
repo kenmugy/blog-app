9413200fb1bfb3763b3f738d273284893b4882e0
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-03 20:31:53 +0300

    Add dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMs',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020',
+    }, 
+    {
+        'author': 'Jane doe',
+        'title': 'Blog Post 1',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020',
+    }, 
+]
+
 def home(request):
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'posts': posts
     }
     return render(request, 'blog/home.html', context)
 
