68b3a43ba1f30b2dc4d311fbe3a0e1aaa6884f98
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-03 19:56:58 +0300

    Add home and about views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        title: 'Home'
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    context = {
+        title: 'About'
+    }
+    return render(request, 'blog/about.html', context)
