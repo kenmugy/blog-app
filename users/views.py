581518f24fffa2aabe44daac43507b18aea35780
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-04 11:42:16 +0300

    Add error message feedback to home

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages import success
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            success(request, f'Account created for {username}')
+            # form.save()
+            return redirect('home')
+
     return render(request,'users/register.html', {'form': form, 'title': 'Register' } )
     
 def login(request):
