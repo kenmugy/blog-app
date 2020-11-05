85dc85e12851cf379093d1b8b56f8a0f7e1cfcfd
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-05 23:10:55 +0300

    Update user registerform to incoporate email field

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import success
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm(request.POST or None)
+    form = UserRegisterForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             username = form.cleaned_data.get('username')
