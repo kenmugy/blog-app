c895285ce58819a62d9779f39d7e565f26b49156
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-07 17:15:39 +0300

    Add new post functionality to enable us create a new blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, about, PostListView,PostDetailView
+from .views import home, about, PostListView,PostDetailView, PostCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home' ),
     path('detail/<int:pk>', PostDetailView.as_view(), name='detail' ),
+    path('new/', PostCreateView.as_view(), name='create' ),
     path('about/', about, name='about' ),
 ]
